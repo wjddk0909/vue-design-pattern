5a40536d813b1a15e87593e4b502a2242a03faad
wjddk0909 <fjddkcb@naver.com>
2022-05-24 23:14:30 +0900

    component with slot - slot vs pops

--- design/slots/src/App.vue
+++ design/slots/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <ul>
-      <item>아이템 1</item>
-      <item>아이템 2</item>
-      <item>아이템 3</item>
-      <item>아이템 4</item>
-      <item>아이템 5</item>
+<!--      <item>아이템 1</item>-->
+<!--      <item>아이템 2</item>-->
+<!--      <item>아이템 3</item>-->
+<!--      <item>아이템 4</item>-->
+<!--      <item>아이템 5</item>-->
+      <item v-for="item in items" :key="item.index" :item="item"></item>
     </ul>
   </div>
 </template>
@@ -16,6 +17,11 @@ import Item from './Item.vue';
 export default {
   components: {
     Item,
+  },
+  data() {
+    return {
+      items: ['아이템1', '아이템2', '아이템3', '아이템4', '아이템5']
+    }
   },
 }
 </script>
--- design/slots/src/Item.vue
+++ design/slots/src/Item.vue
@@ -1,8 +1,11 @@
 <template>
   <li>
-<!--    <slot>-->
-      <!-- NOTE: 등록하는 곳에서 정의할 화면 영역 -->
-<!--    </slot>-->
-    item1
+    {{ item }}
   </li>
 </template>
+
+<script>
+export default {
+  props: ['item']
+}
+</script>
