49b5ce4c16b9a8a99442ac99ae8c4d767151f186
wjddk0909 <fjddkcb@naver.com>
2022-05-24 23:28:41 +0900

    component with slot 구현 방법과 활용처2

--- design/slots/src/App.vue
+++ design/slots/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <ul>
-      <item v-for="item in items" :key="item.index">{{ item }}</item>
+      <item>아이템1</item>
+      <item>아이템2 <button>click me</button></item>
+      <item>
+        <div>아이템3</div>
+        <img src="./assets/endgame.png" alt="어벤져스 엔드게임" width="200px">
+      </item>
+      <item>
+        <div style="color: blue; font-size: 20px;">아이템4</div>
+      </item>
     </ul>
   </div>
 </template>
@@ -12,11 +20,6 @@ import Item from './Item.vue';
 export default {
   components: {
     Item,
-  },
-  data() {
-    return {
-      items: ['아이템1', '아이템2', '아이템3', '아이템4', '아이템5']
-    }
   },
 }
 </script>
