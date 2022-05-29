06f526bf8dc45a27480031cf6b2c680e79d264f2
wjddk0909 <fjddkcb@naver.com>
2022-05-29 23:11:20 +0900

    Controlled Component - 구현방법과 활용처2

--- design/controlled/src/components/CheckBox.vue
+++ design/controlled/src/components/CheckBox.vue
@@ -1,15 +1,17 @@
 <template>
+  <!--  v-model input이벤트와 value값으로 조작 받을때 value로 받고 input을 올린다...?-->
   <input type="checkbox" :value="value" @click="toggleCheckBox">
 </template>
 
 <script>
 export default {
+  // props: ['checked'],
   // @input 이벤트
   // :value 값
-  props: ['value'],
+  props: ['value'], // v-model이 일단은 value로 내려옴
   methods: {
     toggleCheckBox() {
-      this.$emit('input', !this.value);
+      this.$emit('input', !this.value)
     }
   }
 }
